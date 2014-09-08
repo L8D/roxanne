d426508356689391dcc7e9a8ddd429ca37bd7411
Tenor Biel <tenorbiel@gmail.com>
2014-09-08 12:59:58 -0500

    Create roxanne.js

---
+++ roxanne.js
@@ -0,0 +1,13 @@
+(function(root, factory) {
+  if (typeof define === 'function') {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.Roxanne = factory();
+  }
+})(this, function() {
+  var Roxanne = {};
+
+  return Roxanne;
+});
